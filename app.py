0b749c42196519555280b1b985249f630bc76e0b
pratgr <113162958+pratgr@users.noreply.github.com>
2024-05-21 11:44:25 +0530

    Misc changes

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import streamlit as st
 from PyPDF2 import PdfReader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.text_splitter import RecursiveCharacterTextSplitter
 import os
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import google.generativeai as genai
-from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.chains.question_answering import load_qa_chain
-from langchain.prompts import PromptTemplate
+from langchain_community.chains.question_answering import load_qa_chain
+from langchain_community.prompts import PromptTemplate
 from dotenv import load_dotenv
 
 load_dotenv()
